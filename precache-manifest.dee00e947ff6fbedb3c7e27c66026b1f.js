1bf6d69b5d42ee167a1240412e2b31cea18f4f2e
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2021-05-12 05:11:46 +0100

    Updates

---
+++ precache-manifest.dee00e947ff6fbedb3c7e27c66026b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b04e79a142120165d4bd20812863cc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a46b98ec0a1db70a5c7f",
+    "url": "./static/css/main.35ebc145.chunk.css"
+  },
+  {
+    "revision": "409e84fc37c911dd00ae",
+    "url": "./static/js/2.e8b6350d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8b6350d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46b98ec0a1db70a5c7f",
+    "url": "./static/js/main.2fcd4767.chunk.js"
+  },
+  {
+    "revision": "db8a575b665460070412",
+    "url": "./static/js/runtime-main.da83eb2c.js"
+  }
+]);
